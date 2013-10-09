80ce2a5bea174d47160dd4e0196c1d281eb52cf1
thilong@126.com <thilong126>
2013-10-09 17:52:33 +0800

    project file commit

---
+++ libirecovery/libirecovery.cpp
@@ -0,0 +1,12 @@
+// libirecovery.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "libirecovery.h"
+
+// This is an example of an exported function.
+LIBIRECOVERY_API int fnlibirecovery(void)
+{
+	return 42;
+}
+
+
